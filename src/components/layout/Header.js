4a1bd41c4dc7e3a873c0755b343faec33b2d9602
Cameron LaVallie <camlavallie@gmail.com>
2019-06-12 14:51:57 -0500

    styled header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -12,13 +12,13 @@ class Header extends Component {
           <Navbar.Brand href="/home" style={{marginLeft:'25px', fontFamily:'Cormorant', fontSize:'35px'}}>Legacy TEAMS<br/><p style={{fontSize:"15px", fontFamily:'Cormorant', opacity:".8", marginTop:'5px', marginBottom:'5px'}}>Mentoring, Coaching and Pastoral Counseling.</p></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="mr-auto" style={{paddingLeft:'100px', fontSize:'20px', color:'white, imortant!'}}>
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
           <Nav.Link href="/services">Services</Nav.Link>
         </Nav>
-        <Form inline>
+        <Form inline style={{paddingLeft:'90px'}}>
           <a href="https://www.facebook.com/legacyteams.inc/" rel="noopener noreferrer"
           target="_blank"><i className="fa fa-facebook-square" style={{fontSize:"35px"}}></i></a>
 
