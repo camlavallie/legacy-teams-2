0f7bbba6c9a3ab9d94a047afb8775988ef785e53
Cameron LaVallie <camlavallie@gmail.com>
2019-09-17 12:04:19 -0500

    css fixes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -166,19 +166,25 @@ class Home extends Component {
        <Row>
             <Col sm={12}>
                 <Container>
-                  <h1 className="display-4">Frequently Asked Questions (FAQ)</h1>
-                
-                    <h5 className="faq-head">WHAT'S THE BEST WAY TO CONTACT LEGACY TEAMS?</h5>
+                  <h1 className="display-4" style={{textAlign:"center", margin:'20px'}}>Frequently Asked Questions (FAQ)</h1>
+                  <Card body className="faq-card">
+                    <h3 className="faq-head">WHAT'S THE BEST WAY TO CONTACT LEGACY TEAMS?</h3><hr/>
                     <p>Simply email us at chaplainegert@yahoo.com. We'll get back to you within 24 hours.</p><br/>
+                    </Card>
 
-                    <h5 className="faq-head">I’M MARRIED. IS IT NECESSARY THAT MY SPOUSE ALSO PARTICIPATE IN MEETINGS WITH LEGACY TEAMS?</h5>
+                    <Card body className="faq-card">
+                    <h3 className="faq-head">I’M MARRIED. IS IT NECESSARY THAT MY SPOUSE ALSO PARTICIPATE IN MEETINGS WITH LEGACY TEAMS?</h3><hr/>
                     <p>No. You are welcome to contact us privately, and we can work with you individually. In most cases, however, we want to work with both spouses to achieve marital and family wellness.</p><br/>
-
-                    <h5 className="faq-head">WHAT SHOULD I EXPECT FROM LEGACY TEAMS IN TERMS OF ASSISTANCE?</h5>
+                    </Card>
+                    <Card body className="faq-card">
+                    <h3 className="faq-head">WHAT SHOULD I EXPECT FROM LEGACY TEAMS IN TERMS OF ASSISTANCE?</h3><hr/>
                     <p>Much of your work with Legacy TEAMS can be conducted by calls and teleconferencing. Pending the needs of an individual or couple, we may recommend a marriage engagement (face to face intensive) with the location to be determined. Engagements vary in length from two to six days with proportionate costs.</p><br/>
+                    </Card>
 
-                    <h5 className="faq-head">HOW MUCH DOES IT COST TO WORK WITH LEGACY TEAM?</h5>
+                    <Card body className="faq-card">
+                    <h3 className="faq-head">HOW MUCH DOES IT COST TO WORK WITH LEGACY TEAMS?</h3><hr/>
                     <p>Costs vary based on whether one or two individuals (i.e., a couple) are receiving assistance and which members of Legacy TEAMS are providing care. A schedule of fees will be forwarded upon request. Fees may fluctuate based on the situation of individuals. Legacy TEAMS receives donations and gifts to keep ministry costs affordable.</p>
+                    </Card>
                 </Container>
     
             </Col>
