4ef620dbbe3020ee9bd74974053286ed168e5fde
Cameron LaVallie <camlavallie@gmail.com>
2019-10-07 22:23:28 -0500

    update

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,14 +10,14 @@ import { logout } from '../../actions/auth';
 const Header = ({ auth: { isAuthenticated, loading}, logout }) => { 
     const authLinks = (
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" style={{paddingLeft:'100px', fontSize:'20px', color:'white, imortant!'}}>
+        <Nav className="mr-auto" style={{ fontSize:'20px', color:'white, imortant!'}}>
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
-          {/* <Nav.Link href="/posts">Resources</Nav.Link> */}
+          <Nav.Link href="/posts">Resources</Nav.Link>
           <Nav.Link onClick={logout} href="/home">Logout</Nav.Link>
         </Nav>
-        <Form inline style={{paddingLeft:'90px'}}>
+        <Form inline style={{}}>
           <a href="https://www.facebook.com/legacyteams.inc/" rel="noopener noreferrer"
           target="_blank"><i className="fa fa-facebook-square" style={{fontSize:"35px", color:'grey'}}></i></a>
 
@@ -29,20 +29,20 @@ const Header = ({ auth: { isAuthenticated, loading}, logout }) => {
     );
     const guestLinks = (
        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" style={{paddingLeft:'100px', fontSize:'20px', color:'white, imortant!'}}>
+        <Nav className="mr-auto" style={{ fontSize:'20px', color:'white, imortant!'}}>
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
           {/* <Nav.Link href="/posts">Resources</Nav.Link> */}
         </Nav>
-        <Form inline style={{paddingLeft:'90px'}}>
+        {/* <Form inline>
           <a href="https://www.facebook.com/legacyteams.inc/" rel="noopener noreferrer"
           target="_blank"><i className="fa fa-facebook-square" style={{fontSize:"35px", color:'grey'}}></i></a>
 
           < a href= "https://www.instagram.com"
           rel= "noopener noreferrer"
           target="_blank"><i className="fa fa-instagram" style={{fontSize:"35px", color:'grey'}}></i></a>
-    </Form>
+    </Form> */}
       </Navbar.Collapse>
       
     );
@@ -51,12 +51,20 @@ const Header = ({ auth: { isAuthenticated, loading}, logout }) => {
     return (
       <div>
         <Navbar className="navbar"  expand="lg" >
-          <Navbar.Brand href="/home" style={{marginLeft:'25px', fontSize:'35px'}}>LegacyTEAMS<br/><p style={{fontSize:"14px", fontFamily:'Cormorant', opacity:".8", marginTop:'5px', marginBottom:'5px'}}>Transforming, Encouraging, and Assessing Mission Strength.</p></Navbar.Brand>
+          <Navbar.Brand href="/home" style={{marginLeft:'1px', paddingRight:'170px', fontSize:'35px'}}>LegacyTEAMS<br/><p style={{fontSize:"13px", fontFamily:'Cormorant', opacity:".8", marginTop:'1px', marginBottom:'5px'}}>Transforming, Encouraging, & Assessing Mission Strength.</p></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" style={{paddingLeft:'100px', fontSize:'20px', color:'white, imortant!'}}>
+        <Nav className="mr-auto" style={{ paddingLeft:'15px', fontSize:'25px', color:'white'}}>
           { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks}</Fragment>) }
         </Nav>
+        <Form inline>
+          <a href="https://www.facebook.com/legacyteams.inc/" rel="noopener noreferrer"
+          target="_blank"><i className="fa fa-facebook-square" style={{fontSize:"35px", color:'grey'}}></i></a>
+
+          < a href= "https://www.instagram.com"
+          rel= "noopener noreferrer"
+          target="_blank"><i className="fa fa-instagram" style={{fontSize:"35px", color:'grey'}}></i></a>
+    </Form>
        
   </Navbar.Collapse>
 </Navbar>
