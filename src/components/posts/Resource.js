ec285a4996d29f90a91bbcbdcd2671944fb5f8b8
Cameron LaVallie <camlavallie@gmail.com>
2019-11-28 08:23:58 -0600

    css fixes

--- src/components/posts/Resource.js
+++ src/components/posts/Resource.js
@@ -74,63 +74,19 @@ class Resource extends Component {
   </InputGroup>
         <div>{this.state.filteredData.map(i => 
           <div key={i}>
-          <Container style={{textAlign:'center', MarginTop: 100}}>
-
-
-            <div class="lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" title="Woman holding a mug">
-  </div>
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <p class="text-sm text-gray-600 flex items-center">
-        <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-        </svg>
-        {i.name}
-      </p>
-      <div class="text-gray-900 font-bold text-xl mb-2">{i.title}</div>
-      <p class="text-gray-700 text-base">Posted on <Moment format='YYYY/MM/DD'>{i.date}</Moment></p>
-    </div>
-    <div class="flex items-center">
-      <img class="w-20 h-25 rounded-full mr-4" src={i.avatar} alt="Avatar of Jonathan Reinink"/>
-      <div class="text-sm">
-        <p class="text-gray-900 leading-none"> <Linkify>{i.resource}</Linkify></p><hr/>
-        <p class="text-gray-600"> {i.text}</p>
-      </div>
+          <Container>
+           <div class="md:flex bg-white rounded-lg p-5 m-10">
+    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src={i.avatar} />
+    < div class = "text-center md:text-left" >
+      <h2 class="text-lg">{i.name}</h2>
+      <div class="text-purple-500"><a class="text-blue-500" href={i.resource}>{i.title}</a></div>
+      <div class="text-gray-400"> Posted on <Moment format='YYYY/MM/DD'>{i.date}</Moment></div>
+      <div class="text-gray-600">{i.text}</div>
     </div>
   </div>
-</div>
+ 
+         
           </Container>
-          {/* <Accordion className="bg-white">
-            <Card>
-             <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-               <h4>{i.title}</h4>
-              </Accordion.Toggle>
-                <p className="post-date">
-                      Posted on <Moment format='YYYY/MM/DD'>{i.date}</Moment>
-                </p>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body> 
-               <Linkify>
-                  <h5>
-                    {i.resource}
-                 </h5>
-                </Linkify>
-              </Card.Body>
-           </Accordion.Collapse>
-           <Accordion.Collapse eventKey="0">
-              <Card.Body> 
-               <Linkify>
-                  <p>
-                    {i.text}
-                 </p>
-                </Linkify>
-              </Card.Body>
-           </Accordion.Collapse>
-         </Card>
-       </Accordion> */}
         </div>
         )}
       </div>
